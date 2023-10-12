52a00d6b2de207f6e89cfbd0acc935a88d701c74
Zoltan Bathori <zoltan.bathori@telusinternational.com>
2023-10-12 16:54:47 +0300

    Creating a new component: SchedulerRow.js - so now it's separated from Scheduler.js

--- src/components/Scheduler.js
+++ src/components/Scheduler.js
@@ -1,16 +1,11 @@
-import { realtimeDb } from '../firebase/config';
 import { useState, useReducer, useMemo } from 'react';
-import Swal from 'sweetalert2';
 import { CSVLink } from 'react-csv';
 import { format } from 'date-fns';
-import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 
 import './Scheduler.css';
 import Constants from './Constants';
-import BookSession from './BookSession';
 import TableFilter from './Core/TableFilter';
-import LogEvent from './Core/LogEvent';
-import SessionInfo from './Tooltips/SessionInfo';
+import SchedulerRow from './SchedulerRow';
 
 const filterReducer = (state, event) => {
 
@@ -40,11 +35,8 @@ const filterReducer = (state, event) => {
 
 function Scheduler({ database, setUpdateSession }) {
   const [days, setDays] = useState([]);
-  const [highlightedTimeslots, setHighlightedTimeslots] = useState([]);
-  const [showBookSession, setShowBookSession] = useState(false);
-  const [selectedSessionId, setSelectedSessionId] = useState("");
-  const [justBookedSession, setJustBookedSession] = useState("");
   const [csvData, setCsvData] = useState([[]]);
+  const [highlightedTimeslots, setHighlightedTimeslots] = useState([]);
   const [filterData, setFilterData] = useReducer(filterReducer, {
     date: [format(new Date(), "yyyy-MM-dd")],
     phase: Constants['phases'],
@@ -72,11 +64,6 @@ function Scheduler({ database, setUpdateSession }) {
     setDays(temp);
   }, [Object.keys(database['timeslots']).length])
 
-  // Update value in DB
-  function updateValue(path, newValue) {
-    realtimeDb.ref(path).update(newValue);
-  }
-
   function getCSVData() {
     let output = [];
 
@@ -87,9 +74,20 @@ function Scheduler({ database, setUpdateSession }) {
       let participant = {};
       // -1, because we don't need the last column...
       for (var c = 0; c < row.cells.length - 1; c++) {
-        if (c == 0 && r == 0) {
-          temp.push('Date');
-          continue;
+        if (r == 0) {
+          if (c == 0) {
+            temp.push('Date');
+            continue;
+          } else if (c == 2) {
+            temp.push('Session status');
+            continue;
+          } else if (c == 3) {
+            temp.push('Participant status');
+            continue;
+          } else if (c == 7) {
+            temp.push('Phase');
+            continue;
+          }
         }
         if (c == 4 && r > 0) {
           let participantID = row.cells[c].innerHTML;
@@ -110,46 +108,6 @@ function Scheduler({ database, setUpdateSession }) {
     return output;
   }
 
-  function cancelSession(sessionId) {
-    Swal.fire({
-      title: "Are you sure?",
-      showCancelButton: true,
-      html: 'By cancelling the session, it will be deleted from the timetable!',
-      confirmButtonText: 'Yes, cancel!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        let path = "/timeslots/" + sessionId;
-        let data = {
-          participant_id: "",
-          status: "",
-          glasses: false,
-          confirmed: "",
-          booked_today: false,
-          remind: false,
-          delayed: false,
-          comments: ""
-        }
-
-        // Set the bonuses to false
-        let bonuses = database['timeslots'][sessionId]['bonus'];
-        if (bonuses) {
-          data['bonus'] = JSON.parse(JSON.stringify(bonuses));
-          Object.keys(bonuses).map(bonusId => {
-            data['bonus'][bonusId]['a'] = false;
-          })
-        }
-
-        updateValue(path, data);
-
-        LogEvent({
-          pid: database['timeslots'][sessionId]['participant_id'],
-          timeslot: sessionId,
-          action: "Cancel session"
-        })
-      }
-    })
-  }
-
   function filterFunction(timeslotId) {
     const timeslotDate = timeslotId.substring(0, 4) + "-" + timeslotId.substring(4, 6) + "-" + timeslotId.substring(6, 8);
     const session = database['timeslots'][timeslotId];
@@ -165,50 +123,6 @@ function Scheduler({ database, setUpdateSession }) {
       filterData['sessionStatuses'].includes(sessionStatus) &&
       filterData['phase'].includes(phase) &&
       filterData['date'].includes(timeslotDate);
-  }
-
-  function sendReminder(timeslotId) {
-    let pid = database['timeslots'][timeslotId]['participant_id'];
-    let participantInfo = database['participants'][pid];
-    Swal.fire({
-      title: "Reminder",
-      showCancelButton: true,
-      confirmButtonText: 'Yes, send',
-      html: "Are you sure?"
-    }).then((result) => {
-      if (result.isConfirmed) {
-        updateValue("/timeslots/" + timeslotId, { remind: false });
-
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbyZ7PUpLz7hTMAiQqw6dTHpGfqqvV5SNABubnLBYb2phZnd2qS_I_fFrgbU9txyv1oxQg/exec';
-        fetch(scriptURL, {
-          method: 'POST',
-          muteHttpExceptions: true,
-          body: JSON.stringify({
-            "pid": pid,
-            "email_kind": "Reminder",
-            "first_name": participantInfo['first_name'],
-            "last_name": participantInfo['last_name'],
-            "email": participantInfo['email'],
-            "document_request": "",
-            "registration_type": "",
-            "date_of_birth": "",
-            "appointment": timeslotId
-          })
-
-        }).then(res => {
-          Swal.fire({
-            toast: true,
-            icon: 'success',
-            title: 'Reminder sent',
-            animation: false,
-            position: 'bottom',
-            width: 'unset',
-            showConfirmButton: false,
-            timer: 2000
-          })
-        });
-      }
-    })
   }
 
   return (
@@ -274,72 +188,17 @@ function Scheduler({ database, setUpdateSession }) {
               .filter(timeslotId => filterFunction(timeslotId))
               .sort((a, b) => (a.length == 15 ? (a.substring(0, 14) + "0" + a.substring(14)) : a) < (b.length == 15 ? (b.substring(0, 14) + "0" + b.substring(14)) : b) ? -1 : 1)
               .map((key, index, array) => {
-                const participantId = database['timeslots'][key]['participant_id'];
-                const participantInfo = database['participants'][participantId] || {};
-                return (
-                  <tr key={"schedule-row-" + index} className={(justBookedSession == key ? "highlighted-session-row" : "") + (index < array.length - 1 ? (key.substring(0, 13) != array[index + 1].substring(0, 13) ? " day-separator" : "") : "")}>
-                    <td className="center-tag no-wrap">
-                      {key.substring(0, 4) + "-" + key.substring(4, 6) + "-" + key.substring(6, 8) + " " + Constants['bookingDictionary'][key.substring(9, 11) + ":" + key.substring(11, 13)]}
-                      {database['timeslots'][key]['booked_today'] && format(new Date(), "yyyyMMdd") == key.substring(0, 8) &&
-                        <Tooltip
-                          disableInteractive
-                          TransitionProps={{ timeout: 100 }}
-                          componentsProps={{ tooltip: { sx: { fontSize: '1em' }, } }}
-                          title={
-                            <b><span>The participant booked the session today!</span></b>
-                          }
-                        >
-                          <label className="extra-info fas fa-info-circle" />
-                        </Tooltip>
-                      }
-
-                    </td>
-                    <td className={"center-tag no-wrap" + (database['timeslots'][key]['backup'] ? " backup-timeslot" : "")}>
-                      {(database['timeslots'][key]['backup'] ? "Backup" : ("St. " + key.substring(14)))}
-                    </td>
-
-                    <td className="center-tag">
-                      {database['timeslots'][key]['status']}
-                    </td>
-                    <td className="center-tag">
-                      {participantInfo['status'] || ""}
-                    </td>
-                    {database['timeslots'][key]['participant_id'] ? <SessionInfo database={database} participantId={database['timeslots'][key]['participant_id']} sessionId={key} /> : <td></td>}
-                    <td>
-                      {participantId ? (participantInfo['first_name'] + " " + participantInfo['last_name']) : ""}
-                      {participantId &&
-                        <a className="copy-email-link fas fa-search"
-                          title="Google"
-                          target="_blank"
-                          href={("https://www.google.com/search?q=" + participantInfo['first_name'] + " " + participantInfo['last_name'] + " Los Angeles").replaceAll(" ", "%20")}
-                        />}
-                    </td>
-                    <td className={"center-tag " + ((highlightedTimeslots[key.substring(0, 13)] > 4 && database['timeslots'][key]['glasses']) ? "glasses-highlighted" : "")}>
-                      {participantId ?
-                        participantInfo['vision_correction']
-                        : ""}
-                    </td>
-                    <td className="center-tag">
-                      {participantInfo['phase'] ? "Phase " + participantInfo['phase'] : ""}
-                    </td>
-                    <td>
-                      {database['timeslots'][key]['comments']}
-                    </td>
-                    <td className="center-tag">
-                      <div className="buttons-of-timeslot">
-                        {database['timeslots'][key]['status'] == "" && <button className="update-timeslot-button book-button" onClick={() => { setSelectedSessionId(key); setShowBookSession(true) }}>Schedule</button>}
-                        {database['timeslots'][key]['status'] == "Scheduled" && database['timeslots'][key]['remind'] == true && <button className="update-timeslot-button remind-button" onClick={() => sendReminder(key)}>Remind</button>}
-                        {database['timeslots'][key]['status'] != "" && <button className="update-timeslot-button update-button" onClick={() => setUpdateSession(key)}>Update</button>}
-                        {database['timeslots'][key]['status'] != "" && <button className="update-timeslot-button cancel-button" onClick={() => cancelSession(key)}>Cancel</button>}
-                      </div>
-                    </td>
-                  </tr>
-                )
+                return <SchedulerRow
+                  database={database}
+                  sessionId={key}
+                  index={index}
+                  array={array}
+                  setUpdateSession={setUpdateSession}
+                  highlightedTimeslots={highlightedTimeslots} />
               })}
           </tbody>
         </table>
       </div>
-      {showBookSession && <BookSession database={database} setShowBookSession={setShowBookSession} selectedSessionId={selectedSessionId} setJustBookedSession={setJustBookedSession} />}
     </div>
   );
 }
---
+++ src/components/SchedulerRow.js
@@ -0,0 +1,177 @@
+import { realtimeDb } from '../firebase/config';
+import { useState, useReducer, useMemo } from 'react';
+import Swal from 'sweetalert2';
+import { format } from 'date-fns';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+
+import './Scheduler.css';
+import Constants from './Constants';
+import BookSession from './BookSession';
+import LogEvent from './Core/LogEvent';
+import SessionInfo from './Tooltips/SessionInfo';
+
+
+function SchedulerRow({ database, sessionId, index, array, setUpdateSession, highlightedTimeslots }) {
+
+  const [showBookSession, setShowBookSession] = useState(false);
+  const [selectedSessionId, setSelectedSessionId] = useState("");
+  const [justBookedSession, setJustBookedSession] = useState("");
+
+  const participantId = database['timeslots'][sessionId]['participant_id'];
+  const participantInfo = database['participants'][participantId] || {};
+
+  // Update value in DB
+  function updateValue(path, newValue) {
+    realtimeDb.ref(path).update(newValue);
+  }
+
+  function cancelSession(sessionId) {
+    Swal.fire({
+      title: "Are you sure?",
+      showCancelButton: true,
+      html: 'By cancelling the session, it will be deleted from the timetable!',
+      confirmButtonText: 'Yes, cancel!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let path = "/timeslots/" + sessionId;
+        let data = {
+          participant_id: "",
+          status: "",
+          glasses: false,
+          confirmed: "",
+          booked_today: false,
+          remind: false,
+          delayed: false,
+          comments: ""
+        }
+
+        // Set the bonuses to false
+        let bonuses = database['timeslots'][sessionId]['bonus'];
+        if (bonuses) {
+          data['bonus'] = JSON.parse(JSON.stringify(bonuses));
+          Object.keys(bonuses).map(bonusId => {
+            data['bonus'][bonusId]['a'] = false;
+          })
+        }
+
+        updateValue(path, data);
+
+        LogEvent({
+          pid: database['timeslots'][sessionId]['participant_id'],
+          timeslot: sessionId,
+          action: "Cancel session"
+        })
+      }
+    })
+  }
+
+
+  function sendReminder(timeslotId) {
+    let pid = database['timeslots'][timeslotId]['participant_id'];
+    let participantInfo = database['participants'][pid];
+    Swal.fire({
+      title: "Reminder",
+      showCancelButton: true,
+      confirmButtonText: 'Yes, send',
+      html: "Are you sure?"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        updateValue("/timeslots/" + timeslotId, { remind: false });
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbyZ7PUpLz7hTMAiQqw6dTHpGfqqvV5SNABubnLBYb2phZnd2qS_I_fFrgbU9txyv1oxQg/exec';
+        fetch(scriptURL, {
+          method: 'POST',
+          muteHttpExceptions: true,
+          body: JSON.stringify({
+            "pid": pid,
+            "email_kind": "Reminder",
+            "first_name": participantInfo['first_name'],
+            "last_name": participantInfo['last_name'],
+            "email": participantInfo['email'],
+            "document_request": "",
+            "registration_type": "",
+            "date_of_birth": "",
+            "appointment": timeslotId
+          })
+
+        }).then(res => {
+          Swal.fire({
+            toast: true,
+            icon: 'success',
+            title: 'Reminder sent',
+            animation: false,
+            position: 'bottom',
+            width: 'unset',
+            showConfirmButton: false,
+            timer: 2000
+          })
+        });
+      }
+    })
+  }
+
+  return (
+    <tr key={"schedule-row-" + index} className={(justBookedSession == sessionId ? "highlighted-session-row" : "") + (index < array.length - 1 ? (sessionId.substring(0, 13) != array[index + 1].substring(0, 13) ? " day-separator" : "") : "")}>
+      <td className="center-tag no-wrap">
+        {sessionId.substring(0, 4) + "-" + sessionId.substring(4, 6) + "-" + sessionId.substring(6, 8) + " " + Constants['bookingDictionary'][sessionId.substring(9, 11) + ":" + sessionId.substring(11, 13)]}
+        {database['timeslots'][sessionId]['booked_today'] && format(new Date(), "yyyyMMdd") == sessionId.substring(0, 8) &&
+          <Tooltip
+            disableInteractive
+            TransitionProps={{ timeout: 100 }}
+            componentsProps={{ tooltip: { sx: { fontSize: '1em' }, } }}
+            title={
+              <b><span>The participant booked the session today!</span></b>
+            }
+          >
+            <label className="extra-info fas fa-info-circle" />
+          </Tooltip>
+        }
+
+      </td>
+      <td className={"center-tag no-wrap" + (database['timeslots'][sessionId]['backup'] ? " backup-timeslot" : "")}>
+        {(database['timeslots'][sessionId]['backup'] ? "Backup" : ("St. " + sessionId.substring(14)))}
+      </td>
+
+      <td className="center-tag">
+        {database['timeslots'][sessionId]['status']}
+      </td>
+      <td className="center-tag">
+        {participantInfo['status'] || ""}
+      </td>
+      {database['timeslots'][sessionId]['participant_id'] ?
+        <SessionInfo database={database} participantId={database['timeslots'][sessionId]['participant_id']} sessionId={sessionId} />
+        : <td></td>}
+      <td>
+        {participantId ? (participantInfo['first_name'] + " " + participantInfo['last_name']) : ""}
+        {participantId &&
+          <a className="copy-email-link fas fa-search"
+            title="Google"
+            target="_blank"
+            href={("https://www.google.com/search?q=" + participantInfo['first_name'] + " " + participantInfo['last_name'] + " Los Angeles").replaceAll(" ", "%20")}
+          />}
+      </td>
+      <td className={"center-tag " + ((highlightedTimeslots[sessionId.substring(0, 13)] > 4 && database['timeslots'][sessionId]['glasses']) ? "glasses-highlighted" : "")}>
+        {participantId ?
+          participantInfo['vision_correction']
+          : ""}
+      </td>
+      <td className="center-tag">
+        {participantInfo['phase'] ? "Phase " + participantInfo['phase'] : ""}
+      </td>
+      <td>
+        {database['timeslots'][sessionId]['comments']}
+      </td>
+      <td className="center-tag">
+        <div className="buttons-of-timeslot">
+          {database['timeslots'][sessionId]['status'] == "" && <button className="update-timeslot-button book-button" onClick={() => { setSelectedSessionId(sessionId); setShowBookSession(true) }}>Schedule</button>}
+          {database['timeslots'][sessionId]['status'] == "Scheduled" && database['timeslots'][sessionId]['remind'] == true && <button className="update-timeslot-button remind-button" onClick={() => sendReminder(sessionId)}>Remind</button>}
+          {database['timeslots'][sessionId]['status'] != "" && <button className="update-timeslot-button update-button" onClick={() => setUpdateSession(sessionId)}>Update</button>}
+          {database['timeslots'][sessionId]['status'] != "" && <button className="update-timeslot-button cancel-button" onClick={() => cancelSession(sessionId)}>Cancel</button>}
+        </div>
+      </td>
+      {showBookSession && <BookSession database={database} setShowBookSession={setShowBookSession} selectedSessionId={selectedSessionId} setJustBookedSession={setJustBookedSession} />}
+    </tr>
+  )
+}
+
+export default SchedulerRow;
